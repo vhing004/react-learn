7533e01086000ea1c4a9305d035e97cc8500ad3f
vhing004 <hvinh12112004@gmail.com>
2025-04-08 11:38:55 +0700

    conditional ouputing

--- src/views/tests/ChildCPN.js
+++ src/views/tests/ChildCPN.js
@@ -1,26 +1,47 @@
 import { Component } from "react";
 
 class ChildCPN extends Component {
-  state = {};
+  state = {
+    show: false,
+  };
+
+  handleShowHide = () => {
+    this.setState({
+      show: !this.state.show,
+    });
+  };
+
   render() {
     // Dịnh nghĩa nhanh nhiều props
     let { name, age, arrJobs } = this.props;
 
     return (
       <>
-        <p>
-          My nameis {name} - {age} years old
-        </p>
-        <ul>
-          {arrJobs.map((item) => (
-            <li key={item.id}>
-              {item.title} - {item.salary} $
-            </li>
-          ))}
-        </ul>
+        {this.state.show ? (
+          <>
+            <p>
+              My nameis {name} - {age} years old
+            </p>
+            <ul>
+              {arrJobs.map((item) => (
+                <li key={item.id}>
+                  {item.title} - {item.salary} $
+                </li>
+              ))}
+            </ul>
+            <button onClick={this.handleShowHide}>Hide information</button>
+          </>
+        ) : (
+          <button onClick={this.handleShowHide}>Show information</button>
+        )}
       </>
     );
   }
 }
 
 export default ChildCPN;
+// so sánh sự khác nhau giăux class component và function component
+// class component có state, có lifecycle, có this
+// function component không có state, không có lifecycle, không có this
+// function component có thể sử dụng hook để quản lý state, lifecycle
+// function component dùng khi không cần quản lý state, lifecycle
